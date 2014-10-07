9b5a7f6e830d6f283a369473821348ae2d8f1801
Peter Nicholson <peterkowalski123@gmail.com>
2014-10-07 17:17:08 +0200

    browser

--- mpc/js/main.js
+++ mpc/js/main.js
@@ -24,4 +24,30 @@ $(document).ready(function() {
 			}, 1000);
 		});
 	});
+
+	$(".box").click(function() {
+		var src = $(this).attr("target");
+		$(".overlay-page").show();
+		$(".browser").fadeIn(500);
+		$(".browser iframe").attr("src", src);
+	});
+
+	$(".browser iframe").load(function() {
+		$(".browser iframe").show();
+		$(".browser #loading").fadeOut(500);
+	});
+
+	$(document).mouseup(function (e) {
+	    var container = $(".browser");
+
+	    if (!container.is(e.target)
+	        && container.has(e.target).length === 0)  {
+	        $(".browser iframe").hide();
+	    	$(".browser iframe").attr("src", "");
+			$(".browser").fadeOut(500, function() {
+				$(".overlay-page").hide();
+				$(".browser #loading").show();
+			});
+	    }
+	});
 });
