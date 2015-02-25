dfa12a681f75684a5b2cb4e6efc014238a0d49cf
Peter Nicholson <peternicholson99@gg.pl>
2015-02-25 06:42:46 +0100

    Added questions

--- gra/data/questions.js
+++ gra/data/questions.js
@@ -42,8 +42,340 @@ var questions = {
 					"correct": true
 				}
 			]
-		}
+		},
+        
+        {
+			"title": "Stosunej masy Księżyca do masy Ziemi wynosi",
+			"answers": [
+				{
+					"content": "A. 1/90",
+					"correct": false
+				},
+				{
+					"content": "B. 1/40",
+					"correct": false
+				},
+				{
+					"content": "C. 1/80",
+					"correct": true
+				},
+				{
+					"content": "D. 2/30",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Jednostką mocy jest",
+			"answers": [
+				{
+					"content": "A. Wat",
+					"correct": true
+				},
+				{
+					"content": "B. Amper",
+					"correct": false
+				},
+				{
+					"content": "C. Joul",
+					"correct": false
+				},
+				{
+					"content": "D. Wolt",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Ilość cieplła, które jest potrzebne do zmiany temperatury ciała o masie 1 kg o 1C lub 1 kelwin to",
+			"answers": [
+				{
+					"content": "A. Ciepło topnienia",
+					"correct": false
+				},
+				{
+					"content": "B. Ciepło parowania",
+					"correct": false
+				},
+				{
+					"content": "C. Ciepło właściwe",
+					"correct": true
+				},
+				{
+					"content": "D. Ciepło krzepnięcia",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Wzór na ciśnienie to",
+			"answers": [
+				{
+					"content": "A. P = F/S",
+					"correct": true
+				},
+				{
+					"content": "B. V = s/t",
+					"correct": false
+				},
+				{
+					"content": "C. s = 1/2at^2",
+					"correct": false
+				},
+				{
+					"content": "D. d = m/V",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Jaki jest wzór na pracę?",
+			"answers": [
+				{
+					"content": "A. P = Fs",
+					"correct": false
+				},
+				{
+					"content": "B. W = Fs",
+					"correct": true
+				},
+				{
+					"content": "C. R = U/I",
+					"correct": false
+				},
+				{
+					"content": "D. x - yi",
+					"correct": true
+				}
+			]
+		},
+        
+        {
+			"title": "Prąd płynie w obwodzie",
+			"answers": [
+				{
+					"content": "A. Otwartym",
+					"correct": false
+				},
+				{
+					"content": "B. Zamkniętym",
+					"correct": true
+				},
+				{
+					"content": "C. Otwartym i zamkniętym",
+					"correct": false
+				},
+				{
+					"content": "D. Żadna z powyższych odpowiedzi jest poprawna",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Woltomierz wpinamy do obwodu",
+			"answers": [
+				{
+					"content": "A. Równolegle",
+					"correct": false
+				},
+				{
+					"content": "B. Szeregowo",
+					"correct": true
+				},
+				{
+					"content": "C. Nie ma to znaczenia",
+					"correct": false
+				},
+				{
+					"content": "D. Żadna z powyższych odpowiedzi jest poprawna",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Materiały, które mają silne właściwości magnetyczne to",
+			"answers": [
+				{
+					"content": "A. Paramagnetyki",
+					"correct": false
+				},
+				{
+					"content": "B. Ferromagnetyki",
+					"correct": true
+				},
+				{
+					"content": "C. Diamagnetyki",
+					"correct": false
+				},
+				{
+					"content": "D. Domeny",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Odległość, na jaką rozchodzi się fala w czasie wykonania jednego drgania cząsteczki to",
+			"answers": [
+				{
+					"content": "A. Okres fali",
+					"correct": false
+				},
+				{
+					"content": "B. Czoło fali",
+					"correct": false
+				},
+				{
+					"content": "C. Długość fali",
+					"correct": true
+				},
+				{
+					"content": "D. Amplituda fali",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Prędkość światła w próżni wynosi",
+			"answers": [
+				{
+					"content": "A. 300 000 km/h",
+					"correct": false
+				},
+				{
+					"content": "B. 3 m/s",
+					"correct": false
+				},
+				{
+					"content": "C. 300 000 m/s",
+					"correct": true
+				},
+				{
+					"content": "D. 300 000 000 m/s",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Prawo, które mówi nam że kąt padania światła jest równy kątowi odbicia, to:",
+			"answers": [
+				{
+					"content": "A. Prawo Pascala",
+					"correct": false
+				},
+				{
+					"content": "B. Prawo odbicia",
+					"correct": true
+				},
+				{
+					"content": "C. I zasada dynamiki Newtona",
+					"correct": false
+				},
+				{
+					"content": "D. Zjawiska Dopplera",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Jednostką indukcji magnetycznej jest",
+			"answers": [
+				{
+					"content": "A. Newton",
+					"correct": false
+				},
+				{
+					"content": "B. Lenz",
+					"correct": false
+				},
+				{
+					"content": "C. Tesla",
+					"correct": true
+				},
+				{
+					"content": "D. Ohm",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Newton był",
+			"answers": [
+				{
+					"content": "A. Fizykiem",
+					"correct": true
+				},
+				{
+					"content": "B. Aktorem",
+					"correct": false
+				},
+				{
+					"content": "C. Tańcerzem",
+					"correct": false
+				},
+				{
+					"content": "D. Sprzedawcą jabłek",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "Fale o częstotliwości poniżej 16Hz to",
+			"answers": [
+				{
+					"content": "A. Infradzwięki",
+					"correct": true
+				},
+				{
+					"content": "B. Ultradzwięki",
+					"correct": false
+				},
+				{
+					"content": "C. Dzwięki słyszalne",
+					"correct": false
+				},
+				{
+					"content": "D. Fala mechaniczna, która nie jest słyszalna przez zwięrzęta ani ludzi",
+					"correct": false
+				}
+			]
+		},
+        
+        {
+			"title": "x",
+			"answers": [
+				{
+					"content": "A. x",
+					"correct": false
+				},
+				{
+					"content": "B. x",
+					"correct": true
+				},
+				{
+					"content": "C. x",
+					"correct": false
+				},
+				{
+					"content": "D. x",
+					"correct": false
+				}
+			]
+		},
 	],
+    
+    
 
 	"open": [
 		{
